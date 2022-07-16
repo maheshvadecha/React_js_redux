3b80a7b02b389d320e4f43f1536ac3a12e89e17c
rajesh desai <desairajeshrk36@gmail.com>
2022-07-16 17:56:31 +0530

    First Push

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TestClass from "./components/TestClass"
+import TestFun from "./components/TestFun"
+
+const App = () =>{
+  return (
+    <div>
+      <TestFun />
+      <TestClass />
+    </div>
+  );
+}
+export default App; 
